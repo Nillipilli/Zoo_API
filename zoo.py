fb391d3e7bef4e80d59b16d92e57b6a1c7b26fd3
Nillipilli <100154845+Nillipilli@users.noreply.github.com>
2023-09-13 20:15:34 +0200

    implemented basic animal methods in zoo class

--- zoo.py
+++ zoo.py
@@ -6,12 +6,23 @@ class Zoo:
         self.animals: list[Animal] = []
 
     def add_animal(self, animal: Animal) -> None:
-        self.animals.append(animal)
+        """Add an animal to the zoo, but only if it is not already part 
+        of it"""
+        if isinstance(animal, Animal):
+            if animal not in self.animals:
+                self.animals.append(animal)
 
     def remove_animal(self, animal: Animal) -> None:
-        self.animals.remove(animal)
+        """Remove an animal from the zoo, but only if it is part of the 
+        zoo."""
+        if isinstance(animal, Animal):
+            if animal in self.animals:
+                self.animals.remove(animal)
 
     def get_animal(self, animal_id: str) -> Animal | None:
-        for animal in self.animals:
-            if animal.id == animal_id:
-                return animal
+        """Return an animal, but only if an animal with the given id 
+        exists."""
+        if isinstance(animal_id, str):
+            for animal in self.animals:
+                if animal.id == animal_id:
+                    return animal
