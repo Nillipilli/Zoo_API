b6971b3fec009cc2067ae6d542c6591ea517856c
Nillipilli <100154845+Nillipilli@users.noreply.github.com>
2023-09-13 17:53:11 +0200

    added blueprint for Caretaker and Enclosure

--- animal.py
+++ animal.py
@@ -3,25 +3,56 @@ import datetime
 
 
 class Animal:
-    def __init__(self, age: int, species_name: str, common_name: str):
+    def __init__(self, age: int, species_name: str, common_name: str) -> None:
         self.id: str = str(uuid.uuid4())
         self.age = age
         self.species_name = species_name
         self.common_name = common_name
-        self.enclosure: str = ''
-        self.caretaker: str = ''
+        self.enclosure: Enclosure | None = None
+        self.caretaker: Caretaker | None = None
         self.feeding_record: list[str] = []
         self.medical_record: list[str] = []
-        
+
     def set_home(self):
         pass
-    
+
     def set_caretaker(self):
         pass
-        
+
     def feed(self):
         pass
         # self.feeding_record.append(datetime.datetime.now())
-    
+
     def medical_checkup(self):
+        pass
+
+
+class Caretaker:
+    def __init__(self, name: str, address: str) -> None:
+        self.id: str = str(uuid.uuid4())
+        self.name = name
+        self.address = address
+        self.animals: list[Animal] = []
+
+    def add_animal(self):
+        pass
+
+    def remove_animal(self):
+        pass
+
+
+class Enclosure:
+    def __init__(self, name: str, space: float) -> None:
+        self.name = name
+        self.space = space
+        self.animals: list[Animal] = []
+        self.cleaning_record: list[str] = []
+
+    def add_animal(self):
+        pass
+
+    def remove_animal(self):
+        pass
+
+    def clean(self):
         pass
