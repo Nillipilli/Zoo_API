5b169d4f51dc9f2f02578e56604b573448e7d355
Nillipilli <100154845+Nillipilli@users.noreply.github.com>
2023-09-13 19:09:02 +0200

    fixed .json_encoder

--- zoo_json_utils.py
+++ zoo_json_utils.py
@@ -1,17 +1,32 @@
-from json import JSONEncoder
+import json
 from datetime import date
+from json import JSONEncoder
+from flask.json.provider import JSONProvider
+
+
+# had to add this using this post: https://shorturl.at/qEJZ6
+class CustomJSONProvider(JSONProvider):
+
+    def dumps(self, obj, **kwargs):
+        return json.dumps(obj, **kwargs, cls=ZooJsonEncoder)
+
+    def loads(self, s: str | bytes, **kwargs):
+        return json.loads(s, **kwargs)
 
 
 class ZooJsonEncoder(JSONEncoder):
-    # You may need to adapt this class to deal with different type of objects
     def default(self, obj):
         try:
+            # handle date separately
             if isinstance(obj, date):
                 return obj.isoformat()
+
+            # check if object is iterable
             iterable = iter(obj)
         except TypeError:
             pass
         else:
+            # when object is not iterable
             return list(iterable)
-        # default json encoding using the __dict__
+
         return obj.__dict__
--- zooma.py
+++ zooma.py
@@ -1,16 +1,16 @@
 from flask import Flask, jsonify
 from flask_restx import Api, reqparse, Resource
-from zoo_json_utils import ZooJsonEncoder
-from zoo import Zoo
 
-from animal import Animal
+from zoo import Zoo
+from zoo_json_utils import CustomJSONProvider
+from animal import Animal, Caretaker, Enclosure
 
 my_zoo = Zoo()
 
-zooma_app = Flask(__name__)
-# need to extend this class for custom objects, so that they can be jsonified
-zooma_app.json_encoder = ZooJsonEncoder
-zooma_api = Api(zooma_app)
+app = Flask(__name__)
+# had to change this using this post: https://shorturl.at/qEJZ6
+app.json = CustomJSONProvider(app)
+api = Api(app)
 
 animal_parser = reqparse.RequestParser()
 animal_parser.add_argument('species', type=str, required=True,
@@ -21,9 +21,9 @@ animal_parser.add_argument(
     'age', type=int, required=True, help='The age of the animal, e.g., 12')
 
 
-@zooma_api.route('/animal')
+@api.route('/animal')
 class AddAnimalAPI(Resource):
-    @zooma_api.doc(parser=animal_parser)
+    @api.doc(parser=animal_parser)
     def post(self):
         # get the post parameters
         args = animal_parser.parse_args()
@@ -33,35 +33,35 @@ class AddAnimalAPI(Resource):
         # create a new animal object
         new_animal = Animal(species, name, age)
         # add the animal to the zoo
-        my_zoo.addAnimal(new_animal)
+        my_zoo.add_animal(new_animal)
         return jsonify(new_animal)
 
 
-@zooma_api.route('/animal/<animal_id>')
+@api.route('/animal/<animal_id>')
 class Animal_ID(Resource):
     def get(self, animal_id):
-        search_result = my_zoo.getAnimal(animal_id)
+        search_result = my_zoo.get_animal(animal_id)
         # this is automatically jsonified by flask-restx
         return jsonify(search_result)
 
     def delete(self, animal_id):
-        targeted_animal = my_zoo.getAnimal(animal_id)
+        targeted_animal = my_zoo.get_animal(animal_id)
         if not targeted_animal:
             return jsonify("Animal with ID {animal_id} was not found")
-        my_zoo.removeAnimal(targeted_animal)
+        my_zoo.remove_animal(targeted_animal)
         return jsonify("Animal with ID {animal_id} was removed")
 
 
-@zooma_api.route('/animals')
+@api.route('/animals')
 class AllAnimals(Resource):
     def get(self):
         return jsonify(my_zoo.animals)
 
 
-@zooma_api.route('/animals/<animal_id>/feed')
+@api.route('/animals/<animal_id>/feed')
 class FeedAnimal(Resource):
     def post(self, animal_id):
-        targeted_animal = my_zoo.getAnimal(animal_id)
+        targeted_animal = my_zoo.get_animal(animal_id)
         if not targeted_animal:
             return jsonify("Animal with ID {animal_id} was not found")
         targeted_animal.feed()
@@ -69,4 +69,4 @@ class FeedAnimal(Resource):
 
 
 if __name__ == '__main__':
-    zooma_app.run(debug=False, port=7890)
+    app.run(debug=False, port=7890)
