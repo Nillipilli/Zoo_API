bded3dc53542d78a7ae1d1eaa8e90e58e0019367
Nillipilli <100154845+Nillipilli@users.noreply.github.com>
2023-10-12 13:06:23 +0200

    added caretaker test file and fixtures for it

--- conftest.py
+++ conftest.py
@@ -8,6 +8,11 @@ from zoo import Zoo
 from zoo_objects import Animal, Caretaker, Enclosure
 
 
+@pytest.fixture(scope='session')
+def base_url() -> str:
+    return 'http://127.0.0.1:7890'
+
+
 @pytest.fixture
 def zoo1() -> Zoo:
     return Zoo()
@@ -28,19 +33,6 @@ def animal3() -> Animal:
     return Animal('Pan troglodytes', 'Chimpanzee', 36)
 
 
-@pytest.fixture(scope='session')
-def base_url() -> str:
-    return 'http://127.0.0.1:7890'
-
-
-@pytest.fixture(scope='session')
-def unknown_id() -> str:
-    return str(uuid.uuid4())
-
-@pytest.fixture(scope='session')
-def unknown_id2() -> str:
-    return str(uuid.uuid4())
-
 @pytest.fixture
 def enclosure1() -> Enclosure:
     return Enclosure('Cave1', 125)
@@ -54,3 +46,28 @@ def enclosure2() -> Enclosure:
 @pytest.fixture
 def enclosure3() -> Enclosure:
     return Enclosure('Enclosure753', 4.123)
+
+
+@pytest.fixture
+def caretaker1() -> Caretaker:
+    return Caretaker('Laetitia', 'blond-street 19')
+
+
+@pytest.fixture
+def caretaker2() -> Caretaker:
+    return Caretaker('Siena', 'brown rose hall 2')
+
+
+@pytest.fixture
+def caretaker3() -> Caretaker:
+    return Caretaker('Pitt', 'gameon town 123')
+
+
+@pytest.fixture(scope='session')
+def unknown_id() -> str:
+    return str(uuid.uuid4())
+
+
+@pytest.fixture(scope='session')
+def unknown_id2() -> str:
+    return str(uuid.uuid4())
